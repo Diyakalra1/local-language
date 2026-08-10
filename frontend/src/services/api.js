dcd04c0c388ac48bd9f7ec60ff776aa0c46fc398
Diyakalra1 <158696444+Diyakalra1@users.noreply.github.com>
2026-08-10 11:57:34 +0530

    Add comments to API service methods for clarity

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+// axios is used to send http requests - post get delete 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 
-// Create axios instance
+// Create axios instance with base url and json format
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
@@ -12,6 +12,7 @@ const api = axios.create({
 });
 
 // Add token to requests if available
+// Before sending requests add the bearer to the header (JWS authentication formation)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token');
   if (token) {
@@ -21,21 +22,26 @@ api.interceptors.request.use((config) => {
 });
 
 // Auth API
+
+
 export const authAPI = {
+  // take the userData and reqister the user 
   register: async (userData) => {
     const response = await api.post('/auth/register', userData);
     return response.data;
   },
-  
+  // Login-> take credentials and login the user 
   login: async (credentials) => {
     const response = await api.post('/auth/login', credentials);
     return response.data;
   },
-
+// take email to find the user
   searchUser: async (email) => {
     const response = await api.get(`/auth/search/${email}`);
     return response.data;
   },
+
+  // find user by its id
 
   getUserById: async (userId) => {
     const response = await api.get(`/auth/user/${userId}`);
@@ -45,6 +51,7 @@ export const authAPI = {
 
 // Chat API
 export const chatAPI = {
+  // take the information of two participants to create a conversation room
   createConversation: async (participant1_id, participant2_id) => {
     const response = await api.post('/chat/conversations', {
       participant1_id,
@@ -52,17 +59,18 @@ export const chatAPI = {
     });
     return response.data;
   },
-  
+  // get a conversation
   getConversation: async (conversationId) => {
     const response = await api.get(`/chat/conversations/${conversationId}`);
     return response.data;
   },
-  
+  // send Messages
   sendMessage: async (messageData) => {
     const response = await api.post('/chat/messages', messageData);
     return response.data;
   },
-  
+
+  // get messages from the conversationID
   getMessages: async (conversationId) => {
     const response = await api.get(`/chat/messages/${conversationId}`);
     return response.data;
